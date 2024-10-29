25b80ef8d6a273fa981a0d1cbfcf7dd278defe6f
hylee94 <gmlsendol@gmail.com>
2024-10-29 12:07:56 +0900

    기본세팅

---
+++ src/main/java/com/example/customer_back_end/CustomerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBackEndApplication.class, args);
+	}
+
+}
