28b514846e15ea38265a6061f8120e9ef5f9e34c
hylee94 <gmlsendol@gmail.com>
2024-11-08 14:21:09 +0900

    11

---
+++ src/main/java/com/example/repository/FreeBoard1Projection.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+
+//일부만 가져올 때 FreeBoard1 참고하여 FreeBoard1Projection 만들어야 함
+public interface FreeBoard1Projection{ 
+    // int no => get + No()
+    int getNo();
+
+    String getTitle();
+}
