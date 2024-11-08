921345a7b15c1a53b91718f77fbca00e9593fcc3
123kih <inha759@google.com>
2024-11-08 12:43:04 +0900

    1

---
+++ src/main/java/com/example/entity/Menu.java
@@ -0,0 +1,31 @@
+package com.example.entity;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "menu")
+public class Menu {
+    
+    @Id
+    @Column(name = "menu_no")
+    int menuNo;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id" , referencedColumnName ="store_id")
+     @JsonProperty(access = Access.WRITE_ONLY) //추가할때는 사용 가능 , 조회할때는 표시
+    Store storeId;
+
+    String name;
+
+    int price;
+}
---
+++ src/main/java/com/example/entity/MenuImage.java
@@ -0,0 +1,38 @@
+package com.example.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "menuimage")
+public class MenuImage {
+    
+    @Id
+    @Column(name = "menuimage_no")
+    int menuimageNo;
+
+    String filename;
+
+    String filetype;
+
+    long filesize;
+
+    byte[] filedata;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_no" ,referencedColumnName="menu_no")
+    Menu menuNo;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    @CreationTimestamp
+    Date regdate;
+}
