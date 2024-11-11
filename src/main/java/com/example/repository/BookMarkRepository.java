1110b276a37ad2aa898adc13e60bf2342c4ef046
JSL6 <redpaul933@gmail.com>
2024-11-11 16:45:08 +0900

    4:44

--- src/main/java/com/example/repository/BookMarkRepository.java
+++ src/main/java/com/example/repository/BookMarkRepository.java
@@ -1,14 +1,14 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.BookMark;
-import com.example.entity.CustomerMember;
-import com.example.entity.Store;
+
 
 @Repository
 public interface BookMarkRepository extends JpaRepository<BookMark, Integer>{
-    BookMark findByCustomerAndStore(CustomerMember customer, Store store);  // 고객과 상점으로 즐겨찾기 찾기
-    
+    List<BookMark> findByBookmarkNo(int bookmarkNo);
 }
--- src/main/java/com/example/restcontroller/BookMarkRestController.java
+++ src/main/java/com/example/restcontroller/BookMarkRestController.java
@@ -3,17 +3,14 @@ package com.example.restcontroller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.BookMark;
-import com.example.entity.CustomerMember;
-import com.example.entity.Store;
 import com.example.repository.BookMarkRepository;
-import com.example.repository.CustomerMemberRepository;
-import com.example.repository.StoreRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,9 +20,43 @@ import lombok.RequiredArgsConstructor;
 public class BookMarkRestController {
     
     final BookMarkRepository bookMarkRepository;
-    final CustomerMemberRepository customerMemberRepository;
-    final StoreRepository storeRepository;
 
+
+    // 즐겨찾기 추가 (POST)
+    //127.0.0.1:8080/ROOT/api/bookmark/insert.json
+    // { " customerEmail":{"customerEmail":"id1@test.com"},"storeId":{ " storeId":"store1"}}
+    @PostMapping(value = "/insert.json")
+    public Map<String, Object> insertPOST(@RequestBody BookMark obj) {
+    
+        Map<String, Object> map = new HashMap<>();
+        try {
+            bookMarkRepository.save(obj);
+            System.out.println(obj.toString());
+            // 상태 200 (성공)
+            map.put("status", 200);
+            
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            map.put("status", -1);  // 오류 상태
+        }
+
+        return map;
+    }
+    
+    // 즐겨찾기 삭제 (DeleteMapping)
+    @DeleteMapping(value = "/delete.json")
+    public  Map<String, Object> deletePOST(@RequestBody BookMark obj) {
+        // System.out.println(obj.toString());
+        Map<String, Object> map = new HashMap<>();
+        try {
+            bookMarkRepository.deleteById( obj.getBookmarkNo());
+            map.put("status", 200);
+        } catch (Exception e) {
+            map.put("status", -1);
+        }
+        
+        return map;
+    }
 
     
 
