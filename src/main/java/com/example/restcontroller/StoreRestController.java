887b25ed3a03faa44a3d6856cbdc420eed833baa
pmini15 <0115wjdals@naver.com>
2024-11-19 15:41:39 +0900

    ddd

--- src/main/java/com/example/restcontroller/StoreRestController.java
+++ src/main/java/com/example/restcontroller/StoreRestController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
