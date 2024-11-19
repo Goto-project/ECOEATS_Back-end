9aba63b5f830e507ba0778421d3263bc31914ce1
pmini15 <0115wjdals@naver.com>
2024-11-19 09:56:05 +0900

    dd

--- src/main/java/com/example/restcontroller/StoreViewRestController.java
+++ src/main/java/com/example/restcontroller/StoreViewRestController.java
@@ -1,21 +1,12 @@
 package com.example.restcontroller;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.StoreImage;
