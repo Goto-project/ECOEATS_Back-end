3886f3597b4cfe2d2bef3452e77c245507bb0f98
hylee94 <gmlsendol@gmail.com>
2024-11-21 10:44:22 +0900

    241121 엔티티 수정

--- src/main/java/com/example/entity/OrderView.java
+++ src/main/java/com/example/entity/OrderView.java
@@ -29,7 +29,11 @@ public class OrderView {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     LocalTime endpickup;
 
+    String storeid;
+
     String storename;
+
+    String customeremail;
 
     String customernickname;
 
