b97cd0756a5b6bccc6ec104a1a3cc3841a41e6ba
hylee94 <gmlsendol@gmail.com>
2024-11-27 09:31:11 +0900

    241127 admin

--- src/main/java/com/example/mapper/MemberMapper.java
+++ src/main/java/com/example/mapper/MemberMapper.java
@@ -13,16 +13,28 @@ public interface MemberMapper {
 
     //로그인 처리 => 아이디를 전달받으면 해당하는 정보를 반환
     //아이디 암호 권한
-    @Select({"SELECT id, pw, role FROM member1 WHERE id=#{id}"})
-    public Member1 selectMember1One(String id);
+    // @Select({"SELECT id, pw, role FROM member1 WHERE id=#{id}"})
+    // public Member1 selectMember1One(String id);
 
-    //회원가입 INSERT, UPDATE, DELETE는 반환값이 int로 고정됨
-    @Insert({"INSERT INTO member1(id, pw, name, phone, age, role)",
-            " VALUES(#{id}, #{pw}, #{name}, #{phone}, #{age}, #{role})" })
-    public int insertMember1One(Member1 obj);
+    // //회원가입 INSERT, UPDATE, DELETE는 반환값이 int로 고정됨
+    // @Insert({"INSERT INTO member1(id, pw, name, phone, age, role)",
+    //         " VALUES(#{id}, #{pw}, #{name}, #{phone}, #{age}, #{role})" })
+    // public int insertMember1One(Member1 obj);
 
-    //로그인 처리 -> 아이디를 전달받으면 해당하는 정보를 반환
-    //아이디, 암호, 권한
-    @Select({"SELECT id, pw, role FROM member1 WHERE id=#{id}"})
-    public Member1 selectMemberOne(String id);
+    // //로그인 처리 -> 아이디를 전달받으면 해당하는 정보를 반환
+    // //아이디, 암호, 권한
+    // @Select({"SELECT id, pw, role FROM member1 WHERE id=#{id}"})
+    // public Member1 selectMemberOne(String id);
+
+    // store 테이블에서 사용자 조회
+    @Select("SELECT store_id AS id, password AS pw, 'SELLER' AS role FROM store WHERE store_id = #{username}")
+    Member1 selectMemberFromStore(String username);
+
+    // customer 테이블에서 사용자 조회
+    @Select("SELECT customer_email AS id, password AS pw, 'CUSTOMER' AS role FROM customer_member WHERE customer_email = #{username}")
+    Member1 selectMemberFromCustomer(String username);
+
+    // admin 테이블에서 사용자 조회
+    @Select("SELECT admin_id AS id, password AS pw, 'ADMIN' AS role FROM admin WHERE admin_id = #{username}")
+    Member1 selectMemberFromAdmin(String username);
 }
--- src/main/java/com/example/repository/ReviewRepository.java
+++ src/main/java/com/example/repository/ReviewRepository.java
@@ -2,12 +2,20 @@ package com.example.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Review;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Integer>{
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
     List<Review> findByReviewNo(int reviewNo);
+
+    // storeId를 기준으로 페이징된 리뷰를 조회하는 메소드
+    Page<Review> findReviewsByStoreId(String storeId, Pageable pageable);
+
+    // 특정 가게에 대한 리뷰 개수 조회
+    long countByStoreIdStoreId(String storeId);
 }
