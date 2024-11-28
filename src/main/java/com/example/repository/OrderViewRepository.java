98d9c35c766ab0c80c351281cb9b4de015489b0d
hylee94 <gmlsendol@gmail.com>
2024-11-28 17:09:26 +0900

    241128 데일리메뉴 고객용

--- src/main/java/com/example/repository/OrderViewRepository.java
+++ src/main/java/com/example/repository/OrderViewRepository.java
@@ -1,5 +1,6 @@
 package com.example.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,7 @@ import com.example.entity.OrderView;
 
 @Repository
 public interface OrderViewRepository extends JpaRepository<OrderView, Integer> {
-    
+
      // 고객 이메일을 기준으로 주문 내역 조회
      List<OrderView> findByCustomeremail(String customerEmail);
 
@@ -17,4 +18,9 @@ public interface OrderViewRepository extends JpaRepository<OrderView, Integer> {
      List<OrderView> findByStoreid(String storeId);
 
      List<OrderView> findByCustomeremailAndOrdernumber(String customeremail, String ordernumber);
+
+     List<OrderView> findByCustomeremailAndOrderstatus(String customeremail, String ordernumber);
+
+     public List<OrderView> findByCustomeremailAndOrdertimeBetween(String customerEmail, LocalDateTime start,
+               LocalDateTime end);
 }
